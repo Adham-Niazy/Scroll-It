7449263f68f5573e5f6b7f2f6f7c12c8b8b72977
Adham-Niazy <adhamniazy@gmail.com>
2022-12-12 22:59:02 +0200

    Setup base structure

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-import './style.css'
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+import './style.css';
+
+const canvas = document.querySelector('.canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
