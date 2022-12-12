36a413a7d537c217427ba2f7e097d21c6807069f
Adham-Niazy <adhamniazy@gmail.com>
2022-12-12 23:05:19 +0200

    Generating the Images dynamically based on FrameCount

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ import './style.css';
 const canvas = document.querySelector('.canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const context = canvas.getContext('2d');
+const frameCount = 179;
+
+const currentFrame = (index) => `./public/optimized_blender_imgs/${(index + 1).toString()}.webp`;
+const images = [];
+
+for(let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
