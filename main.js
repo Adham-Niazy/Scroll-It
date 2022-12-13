8655aee336d12d1cf4335fbb8731cf96feb262f1
Adham-Niazy <adhamniazy@gmail.com>
2022-12-13 03:16:45 +0200

    Handle Rendering The Frame in the Canvas

--- main.js
+++ main.js
@@ -12,9 +12,20 @@ const frameCount = 179;
 
 const currentFrame = (index) => `./public/optimized_blender_imgs/${(index + 1).toString()}.webp`;
 const images = [];
+let ball = { frame: 0 };
 
-for(let i = 0; i < frameCount; i++) {
+for (let i = 0; i < frameCount; i++) {
   const img = new Image();
   img.src = currentFrame(i);
   images.push(img);
+}
+
+images[0].onload = render;
+
+function render() {
+  context.canvas.width = images[0].width;
+  context.canvas.height = images[0].height;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[ball.frame], 0, 0);
 }
