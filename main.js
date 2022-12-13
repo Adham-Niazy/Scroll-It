121ac900b9b42ab67417be5f56c8c8dd1f1d6838
Adham-Niazy <adhamniazy@gmail.com>
2022-12-13 03:57:42 +0200

    Register ScrollTrigger in GSAP + Adding the Scrolling Behavior

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+// Any External Plugin Should be registered in GSAP Before Using;
+gsap.registerPlugin(ScrollTrigger); 
 
-import './style.css';
+import '/style.css';
 
 const canvas = document.querySelector('.canvas');
 canvas.width = window.innerWidth;
@@ -10,7 +12,7 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
 const frameCount = 179;
 
-const currentFrame = (index) => `./public/optimized_blender_imgs/${(index + 1).toString()}.webp`;
+const currentFrame = (index) => `/optimized_blender_imgs/${(index + 1).toString()}.webp`;
 const images = [];
 let ball = { frame: 0 };
 
@@ -19,6 +21,37 @@ for (let i = 0; i < frameCount; i++) {
   img.src = currentFrame(i);
   images.push(img);
 }
+
+gsap.to(ball, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: "none",
+  scrollTrigger: {
+    scrub: 0.5,
+    pin: "canvas",
+    end: "500%",
+  },
+  onUpdate: render,
+});
+
+gsap.fromTo(
+  ".headline",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      scrub: 1,
+
+      start: "50%",
+      end: "60%",
+    },
+    onComplete: () => {
+      gsap.to(".headline", { opacity: 0 });
+    },
+  }
+);
 
 images[0].onload = render;
 
